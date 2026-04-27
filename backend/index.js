b79a9cb16bcb5b9d8694bc6fda809bf77ffd61de
Dara Karthikeya <dara.karthikeya@outlook.com>
2026-04-28 00:19:46 +0530

    Fix Firebase and export app for Vercel

--- backend/index.js
+++ backend/index.js
@@ -195,7 +195,4 @@ app.get("/", (req, res) => {
   res.send("FinWise backend running with Firestore");
 });
 
-const PORT = 5000;
-app.listen(PORT, () =>
-  console.log(`Server running on http://localhost:${PORT}`)
-);
+export default app;
